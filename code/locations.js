9ffc1f6087d23192707ead2281f01e4ba829d1a1
daridev <hernandezdarifrancisco@gmail.com>
2022-06-25 18:18:40 -0500

    locations collapsed

--- code/locations.js
+++ code/locations.js
@@ -1,33 +1,19 @@
+const wrapper_locations = document.querySelector (".wrapper-locations")
 const locations_articles = document.querySelectorAll ("article.location")
+const show_button = document.querySelector ("button.show-all")
 
-window.onload = () => show_articles()
-window.onresize = () => show_articles()
+show_button.addEventListener ("click", function (e) {
+    this.classList.toggle ("active")
+    let classes = Array.from(this.classList)
 
-function show_articles () {
-    // Get number of visible articles
-    let visible_articles_num = 4
-    if (window.matchMedia("(max-width: 1100px)").matches) {
-        visible_articles_num = 3
-    } 
-    if (window.matchMedia("(max-width: 750px)").matches) {
-        visible_articles_num = 2
-    } 
-    if (window.matchMedia("(max-width: 470px)").matches) {
-        visible_articles_num = 1
-    }
-
-    // Hide all articles
-    for (article of locations_articles) {
-        article.classList.add ("hide")
-    }
-    
-    // Show visible articles
-    for (const [index, article] of locations_articles.entries()) {
-        if (index < visible_articles_num) {
+    if (classes.length == 2) {
+        // Show all articles
+        wrapper_locations.classList.add ("active")
+        for (article of locations_articles) {
             article.classList.remove ("hide")
-        } else {
-            break
         }
+    } else {
+        // Show first row
+        wrapper_locations.classList.remove ("active")
     }
-}
-
+})
