141677cc8a7f93c0163cdb261b876cae6680f389
daridev <hernandezdarifrancisco@gmail.com>
2022-06-26 19:59:04 -0500

    contact form responsive

---
+++ code/contact-general.js
@@ -0,0 +1,10 @@
+const photo = document.querySelector("img.photo")
+
+window.onload = () => hide_photo()
+window.onresize = () => hide_photo()
+
+function hide_photo () {
+    if (window.matchMedia("(max-width: 600px)").matches) {
+        photo.classList.add ("hide")
+    }
+}
