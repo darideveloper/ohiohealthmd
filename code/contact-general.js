dcf579e92364ca0b642ca19503970f69f6ed3851
daridev <hernandezdarifrancisco@gmail.com>
2022-06-26 20:06:22 -0500

    contact form AOS

--- code/contact-general.js
+++ code/contact-general.js
@@ -6,5 +6,8 @@ window.onresize = () => hide_photo()
 function hide_photo () {
     if (window.matchMedia("(max-width: 600px)").matches) {
         photo.classList.add ("hide")
+    } else {
+        photo.classList.remove ("hide")
+
     }
 }
