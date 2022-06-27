f666a90731e34c4cd727ad7633cbd448cadd647c
daridev <hernandezdarifrancisco@gmail.com>
2022-06-26 20:20:03 -0500

    alert after submit form

---
+++ code/alerts.js
@@ -0,0 +1,12 @@
+window.onload = () => show_alert()
+
+function show_alert () {
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    var thanks = urlParams.get('thanks')
+    if (thanks) {
+        
+        // Create alert
+        Swal.fire('Thank you.', 'I will answer you as soon as possible')
+    }
+}
